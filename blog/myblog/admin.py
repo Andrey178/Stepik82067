4cb52cfdc08ba6158322915f6221595a05cfffac
Andrey178 <santa-andrio@mail.ru>
2022-05-30 00:41:33 +0300

    Create model for post. Add WYSWYG plugin 'django-ckeditor' for post. Connect admin-panel. Create super-user

--- blog/myblog/admin.py
+++ blog/myblog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Post
+
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    # урл в админке будет заполняться автоматически на основании тайтла
+    prepopulated_fields = {'url': ('title',)}
+
+
+
+admin.site.register(Post, PostAdmin)
--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.views import View
 
+from .models import Post
 # Create your views here.
 
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
+        posts = Post.obects.all()
         return render(
             request,
-            'myblog/home.html'
+            'myblog/home.html',
+            context={'post': posts}
         )
