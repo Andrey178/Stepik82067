edd1c0eebcdf326e5941b8a8874427eeddd0cc1b
Andrey178 <santa-andrio@mail.ru>
2022-06-01 22:35:08 +0300

    Add pagination posts and with 'previous' and 'next' links.   Correct MainView in views.py to get posts from base and return one page with posts

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 
+from django.core.paginator import Paginator
+
 from .models import Post
 # Create your views here.
 
@@ -8,4 +10,9 @@ from .models import Post
 class MainView(View):
     def get(self, request, *args, **kwargs):
         posts = Post.objects.all()
-        return render(request, 'myblog/home.html', context={'posts': posts})
+        paginator = Paginator(posts, 6)
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        return render(request, 'myblog/home.html', context={'page_obj': page_obj})
