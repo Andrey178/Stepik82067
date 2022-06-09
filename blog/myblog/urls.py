ec7966268c4cc539981057439f36466006d54c69
Andrey178 <santa-andrio@mail.ru>
2022-06-09 22:34:58 +0300

    Add search page with mock working.

--- blog/myblog/urls.py
+++ blog/myblog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import MainView, PostDetailView, SignUpView, SignInView, FeedBackView, SuccessView
+from .views import MainView, PostDetailView, SignUpView, SignInView, FeedBackView, SuccessView, SearchResultsView
 from django.contrib.auth.views import LogoutView
 from django.conf import settings
 
@@ -10,5 +10,6 @@ urlpatterns = [
     path('signin/', SignInView.as_view(), name='signin'),
     path('signout/', LogoutView.as_view(), {'next_page': settings.LOGOUT_REDIRECT_URL}, name='signout'),
     path('contact/', FeedBackView.as_view(), name='contact'),
-    path('contact/success/', SuccessView.as_view(), name='success')
+    path('contact/success/', SuccessView.as_view(), name='success'),
+    path('search/', SearchResultsView.as_view(), name='search_results'),
 ]
